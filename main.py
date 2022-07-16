c2ee0e3a9dbeb8b9ca267e0756f39777ee047daf
RandyCullins <randy@devpipeline.com>
2022-07-16 10:13:03 -0600

    routes directory added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from db import db, init_db
+
 from models.actors_model import Actors
 from models.directors_model import Directors
 from models.movies_model import Movies
 
-import controllers
+import routes
 
 app = Flask(__name__)
 
@@ -54,105 +55,10 @@ def create_all():
         else:
             print("Top Gun found! Starting Application...")
 
-@app.route('/actors/add', methods=['POST'])
-def add_actor():
-    return controllers.add_actor()
-
-@app.route('/actors/list')
-def get_all_actors():
-    return controllers.get_all_actors()
-
-@app.route('/actors/list/<actor_id>')
-def get_one_actor(actor_id):
-    return controllers.get_one_actor(actor_id)
-
-@app.route('/actors/edit/<actor_id>', methods=['PUT'])
-def edit_actor(actor_id):
-    return controllers.edit_actor(actor_id)
-
-@app.route('/actors/delete/<actor_id>', methods=['DELETE'])
-def delete_actor(actor_id):
-    return controllers.delete_actor(actor_id)
-
-@app.route('/actors/deactivate/<actor_id>', methods=['PUT'])
-def deactivate_actor(actor_id):
-    return controllers.deactivate_actor(actor_id)
-
-@app.route('/actors/activate/<actor_id>', methods=['PUT'])
-def activate_actor(actor_id):
-    return controllers.activate_actor(actor_id)
-
-@app.route('/actor/search/<search_term>')
-def actors_get_by_search(search_term):
-    return controllers.actors_get_by_search(search_term)
-
-@app.route('/directors/add', methods=['POST'])
-def add_director():
-    return controllers.add_director()
-
-@app.route('/directors/list')
-def get_all_directorrs():
-    return controllers.get_all_directors()
-
-@app.route('/directors/list/<director_id>')
-def get_one_director(director_id):
-    return controllers.get_one_director(director_id)
-
-@app.route('/directors/edit/<director_id>', methods=['PUT'])
-def edit_director(director_id):
-    return controllers.edit_director(director_id)
-
-@app.route('/directors/delete/<director_id>', methods=['DELETE'])
-def delete_director(director_id):
-    return controllers.delete_director(director_id)
-
-@app.route('/directors/deactivate/<director_id>', methods=['PUT'])
-def deactivate_director(director_id):
-    return controllers.deactivate_director(director_id)
-
-@app.route('/directors/activate/<director_id>', methods=['PUT'])
-def activate_director(director_id):
-    return controllers.activate_director(director_id)
-
-@app.route('/director/search/<search_term>')
-def directors_get_by_search(search_term):
-    return controllers.directors_get_by_search(search_term)
-
-@app.route('/movies/add', methods=['POST'])
-def movie_add():
-    return controllers.movie_add()
-
-@app.route('/movies/list')
-def get_all_movies():
-    return controllers.get_all_movies()
-
-@app.route('/movies/list/<movie_id>')
-def get_one_movie(movie_id):
-    return controllers.get_one_movie(movie_id)
-
-@app.route('/movies/edit/<movie_id>', methods=['PUT'])
-def edit_movie(movie_id):
-    return controllers.edit_movie(movie_id)
-
-@app.route('/movies/delete/<movie_id>', methods=['DELETE'])
-def delete_movie(movie_id):
-    return controllers.delete_movie(movie_id)
-
-@app.route('/movies/deactivate/<movie_id>', methods=['PUT'])
-def deactivate_movie(movie_id):
-    return controllers.deactivate_movie(movie_id)
-
-@app.route('/movies/activate/<movie_id>', methods=['PUT'])
-def activate_movie(movie_id):
-    return controllers.activate_movie(movie_id)
-
-@app.route('/movie/search/<search_term>')
-def movies_get_by_search(search_term):
-    return controllers.movies_get_by_search(search_term)
-
-@app.route('/search/<search_term>')
-def get_all_by_search(search_term):
-    return controllers.get_all_by_search(search_term)
+app.register_blueprint(routes.actor)
+app.register_blueprint(routes.director)
+app.register_blueprint(routes.movie)
+app.register_blueprint(routes.search)
 
 if __name__ == "__main__":
     create_all()
